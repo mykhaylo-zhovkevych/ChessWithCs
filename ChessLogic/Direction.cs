107007266f5f10a1079406e221b1418d1a345e0b
mykhaylo zhovkevych <148889468+mykhaylo-zhovkevych@users.noreply.github.com>
2025-11-08 19:19:38 +0100

    Projektdateien hinzufügen.

---
+++ ChessLogic/Direction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+
+    /// <summary>
+    /// This class represent direction on the chessboard
+    /// </summary>
+    public class Direction
+    {
+        public readonly static Direction North = new Direction(-1, 0);
+        public readonly static Direction South = new Direction(1, 0);
+        public readonly static Direction East = new Direction(0, 1);
+        public readonly static Direction West = new Direction(0, -1);
+        // Diagonal Directions
+        public readonly static Direction NorthEast = North + East;
+        public readonly static Direction NorthWest = North + West;
+        public readonly static Direction SouthEast = South + East;
+        public readonly static Direction SouthWest = South + West;
+
+        // Represents the change value in row and column for direction
+        public int RowDelta { get; }
+        public int ColumnDelta { get; }
+
+        public Direction(int rowDelta, int columnDelta)
+        {
+            RowDelta = rowDelta;
+            ColumnDelta = columnDelta;
+        }
+
+        public static Direction operator +(Direction dir01, Direction dir02)
+        {
+            return new Direction(dir01.RowDelta + dir02.RowDelta, dir01.ColumnDelta + dir02.ColumnDelta);
+        }
+        public static Direction operator *(int scalar, Direction dir)
+        {
+            return new Direction(scalar * dir.RowDelta, scalar * dir.ColumnDelta);
+        }
+
+
+
+
+    }
+}
---
+++ ChessLogic/Position.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    /// <summary>
+    /// It is taken that thje top-right corner is (0,0)
+    /// </summary>
+    public class Position
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Position (int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+        // It return Player because Player has specifi color value
+        public Player SquareColor()
+        {
+            // If is even
+            if ((Row + Column) % 2 == 0)
+            {
+                return Player.White;
+            }
+            else
+            {
+                return Player.Black;
+            }
+        }
+
+
+        // For creating internal method. Use `ctrl + .`
+        public override bool Equals(object obj)
+        {
+            return obj is Position position &&
+                   Row == position.Row &&
+                   Column == position.Column;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Column);
+        }
+
+        // 
+        public static bool operator ==(Position left, Position right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+
+        // Returns one Position by adding one value of the Direction 
+        public static Position operator +(Position pos, Direction dir)
+        {
+            return new Position(pos.Row + dir.RowDelta, pos.Column + dir.ColumnDelta)
+        }
+    }
+}
