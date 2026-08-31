7f9daf6751d039c1793193090363470b22e27574
wefer <148889468+mykhaylo-zhovkevych@users.noreply.github.com>
2026-08-31 18:14:00 +0200

    added another feature

--- ChessLogic/Moves/CastleMove.cs
+++ ChessLogic/Moves/CastleMove.cs
@@ -40,11 +40,34 @@ namespace ChessLogic.Moves
             }
         }
 
-        // Move both figures
         public override void Execute(Board board)
         {
             new NormalMove(FromPos, ToPos).Execute(board);
             new NormalMove(rookFromPos, rookToPos).Execute(board);
+        }
+
+        public override bool IsLegal(Board board)
+        {
+            Player player = board[FromPos].Color;
+            if (board.IsInCheck(player))
+            {
+                return false;
+            }
+
+            Board copy = board.Copy();
+            Position kingPosInCopy = FromPos;
+
+            for (int i = 0; i < 2; i++ )
+            {
+                new NormalMove(kingPosInCopy, kingPosInCopy + kingMoveDir).Execute(copy);
+                kingPosInCopy += kingMoveDir;
+
+                if (copy.IsInCheck(player))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
